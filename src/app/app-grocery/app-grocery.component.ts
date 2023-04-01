496a5634cc2e43e0ac49af7201422b2761851c00
shelra90 <shelra90@yahoo.com>
2023-04-01 09:28:13 -0400

    Created app-grocery component

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['./app-grocery.component.css']
+})
+export class AppGroceryComponent {
+
+}
+export class GroceryComponent {
+}
